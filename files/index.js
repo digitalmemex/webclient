0c010bee754e19185fdaa35aeccbe71adcd09cb5
dgf <deep@dagnu.de>
2014-06-03 14:24:23 +0200

    rename $body

--- files/index.js
+++ files/index.js
@@ -7,24 +7,24 @@ define(['jQuery', 'dm4rest'], function ($, rest) {
 
   // jQuery ready => start up
   $(function () {
-    var body = $('body');
-    body.text('loading ...');
+    var $body = $('body');
+    $body.text('loading ...');
 
     // query type list
     rest.get_all_topic_types(function (types) {
-      body.empty().append($('<h1>').text('DeepaMehta Topic Types'));
+      $body.empty().append($('<h1>').text('DeepaMehta Topic Types'));
 
       // create HTML list of types
       var $list = $('<ul>');
       types.sort(function (a, b) {
         return a.value > b.value;
       }).forEach(function (type) {
-        var $type = $('<li>').text(type.value + ' ' + type.uri);
+        var $type = $('<li>').text(type.value + ' - ' + type.uri);
         $list.append($type);
       });
 
       // view the type list
-      body.append($list);
+      $body.append($list);
     });
   });
 });
